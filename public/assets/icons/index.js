687427578637d61542083ebfa49fd58a9d876f12
mubasyir19 <m.mubasyir19@gmail.com>
2023-03-12 14:22:50 +0700

    Upload project

---
+++ public/assets/icons/index.js
@@ -0,0 +1,11 @@
+import ICflight from "./flight.svg";
+import ICfhotel from "./hotel.svg";
+import ICtakeoff from "./takeOff.svg";
+import IClanding from "./landing.svg";
+import ICcalendar from "./calendar.svg";
+import ICtype from "./type.svg";
+import IClocation from "./location.svg";
+import ICsearch from "./search.svg";
+import ICstar from "./star.svg";
+
+export { ICcalendar, ICfhotel, ICflight, IClanding, IClocation, ICsearch, ICstar, ICtakeoff, ICtype };
